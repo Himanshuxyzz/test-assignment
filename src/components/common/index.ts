7669eef546428827fd0edb387e27313d00bb068a
Himanshu Toppo <72000638+Himanshuxyzz@users.noreply.github.com>
2026-01-14 15:18:42 +0530

    Update index.ts

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as CustomButton } from "./CustomButton";
 export { default as Container } from "./Container";
 export { SkeletonItem, SkeletonContainer } from "./Skeleton";
+export { default as Header } from "./Header";
