0aeb345a84a94ac8dd8df32480ee59b00c9f4ff8
Himanshu Toppo <72000638+Himanshuxyzz@users.noreply.github.com>
2026-01-14 15:17:54 +0530

    chores : added custom hook for the isLoading

---
+++ src/hooks/useDelayLoading.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+const useDelayLoading = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return isLoading;
+};
+
+export default useDelayLoading;
